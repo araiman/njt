4635957ae8ad308d791adc85f6c220226e5d7643
araiman <araiman@araimans-MacBook-Pro.local>
2020-09-05 12:48:21 +0900

    refactored functions

--- task3/index.js
+++ task3/index.js
@@ -1,71 +1,69 @@
 const taskForm = document.querySelector('.task-form');
-const todos = document.querySelector('.todos');
-
-let tasks = [];
+const todo = document.querySelector('.todo');
 
 const WIP = '作業中';
 const DONE = '完了';
 
-// classなのか、constを使うのがいいのか
-class Task {
-  constructor(id, value) {
-    this.id = id;
-    this.state = WIP;
-    this.value = value;
-  }
+taskForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const taskName = taskForm.task.value.trim();
+
+    if (taskName.length) {
+        todoController.addItem(taskName);
+        taskForm.reset();
+    }
+});
+
+const todoController = {
+    todo: [],
+    addItem: name => {
+        const item = new Task(todoController.todo.length, name);
+        todoController.todo.push(item);
+        todoRenderer.addItem(item);
+    },
+    deleteItem: id => {
+        todoController.todo = todoController.todo.filter(t => t.id !== id).map((t, i) => {
+            t.id = i;
+            return t
+        });
+        todoRenderer.reRender(todoController.todo);
+    }
 }
 
-// TODOなのか、タスクなのか
-const tasksHeader = `
+const todoRenderer = {
+    header: `
       <tr>
         <th>ID</th>
         <th>コメント</th>
         <th>状態</th>
         <th></th>
       </tr>
-`
-
-const taskHtml = task => {
-  return `
-    <tr>
-      <td>${task.id}</td>
-      <td>${task.value}</td>
-      <td><button>${task.state}</button></td>
-      <td><button onclick="deleteItem(${task.id})">削除</button></td>
-    </tr>
-  `
+    `,
+    // インデント変
+    row: task => {
+        return `
+        <tr>
+         <td>${task.id}</td>
+         <td>${task.value}</td>
+         <td><button>${task.state}</button></td>
+         <td><button onclick="todoController.deleteItem(${task.id})">削除</button></td>
+        </tr>
+    `
+    },
+    addItem: item => {
+        todo.innerHTML += todoRenderer.row(item);
+    },
+    reRender: tasks => {
+        const taskList = tasks.reduce((html, task) => html + todoRenderer.row(task), ``);
+        todo.innerHTML = todoRenderer.header + taskList;
+    }
 }
 
-const reRender = tasks => {
-  const taskList = tasks.reduce((html, task) => html + taskHtml(task), ``);
-  todos.innerHTML = tasksHeader + taskList;
+class Task {
+    constructor(id, value) {
+        this.id = id;
+        this.state = WIP;
+        this.value = value;
+    }
 }
-
-// 名前がバラバラ
-const addTodo = task => {
-  tasks.push(task);
-  todos.innerHTML += taskHtml(task);
-}
-
-const deleteItem = id => {
-  // どうインデントするのが正しいのか
-  tasks = tasks.filter(t => t.id !== id).map((t,i) => {
-    t.id = i;
-    return t
-  });
-  reRender(tasks);
-}
-
-
-taskForm.addEventListener('submit', e => {
-  e.preventDefault();
-
-  const taskName = taskForm.task.value.trim();
-
-  if (taskName.length) {
-    // ID発行の責務をどこに持たせるか
-    const task = new Task(tasks.length, taskName);
-    addTodo(task);
-    taskForm.reset();
-  }
-});
