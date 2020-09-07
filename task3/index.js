6b206f48314e24d76449e067b9de770e9be1dc34
araiman <araiman@araimans-MacBook-Pro.local>
2020-09-07 22:58:35 +0900

    reformat code

--- task3/index.js
+++ task3/index.js
@@ -8,65 +8,65 @@ const DONE = '完了';
 const DELETE_BUTTON_VALUE = '削除';
 
 class Task {
-  constructor(id, value) {
-    this.id = id;
-    this.state = WIP;
-    this.value = value;
-  }
+    constructor(id, value) {
+        this.id = id;
+        this.state = WIP;
+        this.value = value;
+    }
 }
 
 const addTask = () => {
-  const value = taskForm.value.trim();
-  if (value.length) {
-    const task = new Task(todos.length, value)
+    const value = taskForm.value.trim();
+    if (value.length) {
+        const task = new Task(todos.length, value)
 
-    todos.push(task);
-    appendToTodoList(task);
+        todos.push(task);
+        appendToTodoList(task);
 
-    taskForm.value = '';
-  }
+        taskForm.value = '';
+    }
 }
 
 const appendToTodoList = task => {
-  const idCell = createCell(task.id);
-  const todoCell = createCell(task.value);
-  const stateCell = createCell(createButton(task.state));
-  const deleteCell = createCell(createButton(DELETE_BUTTON_VALUE));
+    const idCell = createCell(task.id);
+    const todoCell = createCell(task.value);
+    const stateCell = createCell(createButton(task.state));
+    const deleteCell = createCell(createButton(DELETE_BUTTON_VALUE));
 
-  const todo = createRow([idCell, todoCell, stateCell, deleteCell]);
+    const todo = createRow([idCell, todoCell, stateCell, deleteCell]);
 
-  todoList.appendChild(todo);
+    todoList.appendChild(todo);
 }
 
 const createCell = value => {
-  const cell = document.createElement('td');
+    const cell = document.createElement('td');
 
-  switch (typeof value) {
-    case "string":
-      cell.textContent = value;
-      break;
-    case "number":
-      cell.textContent = value;
-      break;
-    default:
-      cell.appendChild(value);
-  }
+    switch (typeof value) {
+        case "string":
+            cell.textContent = value;
+            break;
+        case "number":
+            cell.textContent = value;
+            break;
+        default:
+            cell.appendChild(value);
+    }
 
-  return cell;
+    return cell;
 }
 
 const createButton = value => {
-  const button = document.createElement('button');
-  button.textContent = value;
+    const button = document.createElement('button');
+    button.textContent = value;
 
-  return button;
+    return button;
 }
 
 const createRow = (columns) => {
-  const row = document.createElement('tr');
-  columns.forEach(c => {
-    row.appendChild(c);
-  })
+    const row = document.createElement('tr');
+    columns.forEach(c => {
+        row.appendChild(c);
+    })
 
-  return row;
+    return row;
 }
