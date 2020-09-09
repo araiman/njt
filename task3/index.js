0850e5cf282b2c65ecc912ad4806cdb7336f481a
araiman <araiman@araimans-MacBook-Pro.local>
2020-09-09 22:53:24 +0900

    added delete funtion

--- task3/index.js
+++ task3/index.js
@@ -1,11 +1,20 @@
 const taskForm = document.querySelector('.task-form');
-const todoList = document.querySelector('.todo-list');
+const todoListView = document.querySelector('.todo-list');
 
-const todos = [];
+let todos = [];
 
 const WIP = '作業中';
 const DONE = '完了';
 const DELETE_BUTTON_VALUE = '削除';
+
+const todoListHeader = `
+    <tr>
+        <th>ID</th>
+        <th>コメント</th>
+        <th>状態</th>
+        <th></th>
+    </tr>
+`
 
 class Task {
     constructor(id, value) {
@@ -28,21 +37,22 @@ const addTask = () => {
 }
 
 const appendToTodoList = task => {
+    const todoView = createTodoView(task);
+    todoListView.appendChild(todoView);
+}
+
+const createTodoView = task => {
     const idCell = createCell(task.id);
     const todoCell = createCell(task.value);
     const stateCell = createCell(createButton(task.state));
-    const deleteCell = createCell(createButton(DELETE_BUTTON_VALUE));
+    const deleteCell = createCell(createDeleteButton(task.id));
 
-    const todo = createRow([idCell, todoCell, stateCell, deleteCell]);
-
-    todoList.appendChild(todo);
+    return createRow([idCell, todoCell, stateCell, deleteCell]);
 }
 
-const createRow = (columns) => {
+const createRow = columns => {
     const row = document.createElement('tr');
-    columns.forEach(c => {
-        row.appendChild(c);
-    })
+    columns.forEach(c => row.appendChild(c));
 
     return row;
 }
@@ -62,10 +72,31 @@ const createCell = value => {
     return cell;
 }
 
-const createButton = value => {
+const createButton = text => {
     const button = document.createElement('button');
-    button.textContent = value;
+    button.textContent = text;
 
     return button;
 }
 
+const createDeleteButton = id => {
+    const button = createButton(DELETE_BUTTON_VALUE);
+    button.value = id;
+    button.onclick = deleteTask;
+    return button;
+}
+
+const deleteTask = e => {
+    todos = todos.filter(t => t.id != e.target.value)
+        .map((t, i) => {
+            t.id = i;
+            return t;
+        });
+
+    reRender(todos);
+}
+
+const reRender = todos => {
+    todoListView.innerHTML = todoListHeader;
+    todos.forEach(t => todoListView.appendChild(createTodoView(t)));
+}
